391febfb5c41cc6c3cb7e3148cd4f0ae55f08560
haryu <ryu.axel@gmail.com>
2022-03-13 00:02:42 +0900

    첫 글 작성 완료

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -13,7 +13,7 @@ module.exports = {
   author: {
     name: `류한솔`,
     bio: {
-      role: `야생의 개발자...`,
+      role: `야생의 개발자`,
       description: ['인문학적', '스스로 질문하는', '소통을 중시하는'],
       thumbnail: 'profile.png', // Path to the image in the 'asset' folder
     },
